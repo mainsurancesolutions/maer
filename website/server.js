891cf6c30d571ae1a380867e5786a6176ce62eef
Kirk <Kirk@rwpolicy.com>
2026-07-01 07:08:24 -0400

    Add marketing website and legal disclaimers

---
+++ website/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.use(express.static(path.join(__dirname)))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.listen(PORT, () => {
+  console.log('ContractsCompare website running on port ' + PORT)
+})
